47b48a1abdce076acc4fd8f39382a79d101b7e2f
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-10-25 13:24:22 -0500

    further course updates

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,28 +8,29 @@ import { Subject } from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Peppizza',
-    'A test-recipe',
-    'https://get.pxhere.com/photo/dish-food-cuisine-ingredient-la-carte-food-hors-d-oeuvre-recipe-vegetarian-food-comfort-food-produce-appetizer-Persillade-side-dish-finger-food-meat-garnish-canape-japanese-cuisine-mediterranean-food-1621827.jpg',
-      [
-        new Ingredient('Pepperoni', 37),
-        new Ingredient('Tomato Sauce', 1),
-        new Ingredient('Dough', 1),
-      ]
-    ),
-    new Recipe(
-      'Megatini',
-    'A second test-recipe',
-    'https://get.pxhere.com/photo/dish-food-cuisine-ingredient-la-carte-food-hors-d-oeuvre-recipe-vegetarian-food-comfort-food-produce-appetizer-Persillade-side-dish-finger-food-meat-garnish-canape-japanese-cuisine-mediterranean-food-1621827.jpg',
-      [
-        new Ingredient('Olives', 37),
-      new Ingredient('Gin', 1),
-      new Ingredient('Vermouth', 1),
-    ]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Peppizza',
+  //   'A test-recipe',
+  //   'https://get.pxhere.com/photo/dish-food-cuisine-ingredient-la-carte-food-hors-d-oeuvre-recipe-vegetarian-food-comfort-food-produce-appetizer-Persillade-side-dish-finger-food-meat-garnish-canape-japanese-cuisine-mediterranean-food-1621827.jpg',
+  //     [
+  //       new Ingredient('Pepperoni', 37),
+  //       new Ingredient('Tomato Sauce', 1),
+  //       new Ingredient('Dough', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Megatini',
+  //   'A second test-recipe',
+  //   'https://get.pxhere.com/photo/dish-food-cuisine-ingredient-la-carte-food-hors-d-oeuvre-recipe-vegetarian-food-comfort-food-produce-appetizer-Persillade-side-dish-finger-food-meat-garnish-canape-japanese-cuisine-mediterranean-food-1621827.jpg',
+  //     [
+  //       new Ingredient('Olives', 37),
+  //     new Ingredient('Gin', 1),
+  //     new Ingredient('Vermouth', 1),
+  //   ]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService){}
 
@@ -57,6 +58,11 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 }
